dbc07455916519c99b356a7a1188dc263dbe80b7
Dima <dimakorniets@gmail.com>
2018-05-26 16:36:32 +0300

    +added observing job status

--- app/src/main/java/workmanager/ice6ergteam/com/workmanager/MainActivity.kt
+++ app/src/main/java/workmanager/ice6ergteam/com/workmanager/MainActivity.kt
@@ -1,25 +1,42 @@
 package workmanager.ice6ergteam.com.workmanager
 
+import android.arch.lifecycle.Observer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.work.*
+import timber.log.Timber
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var  mWorkManager: WorkManager
+    private val  mWorkManager: WorkManager by lazy {
+        WorkManager.getInstance()
+    }
+    val PERIODIC_TASK_TAG :String = "PERIODIC_TASK"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(LineNumberDebugTree())
+        }
         setContentView(R.layout.activity_main)
-        mWorkManager = WorkManager.getInstance()
+    }
+
+    class LineNumberDebugTree : Timber.DebugTree() {
+        override fun createStackElementTag(element: StackTraceElement): String? {
+            //remove the extra info and only get the class name.
+            val className = super.createStackElementTag(element)?.split("$")?.get(0)
+            return "($className.kt:${element.lineNumber})#${element.methodName}"
+        }
     }
 
     override fun onStart() {
         super.onStart()
+        mWorkManager.getStatusesByTag(PERIODIC_TASK_TAG).observe(this, Observer {
+            Timber.e("Work status: %s", it)
+        })
         findViewById<Button>(R.id.button).setOnClickListener {
+            mWorkManager.cancelAllWorkByTag(PERIODIC_TASK_TAG)
             startWork()
         }
     }
@@ -27,14 +44,15 @@ class MainActivity : AppCompatActivity() {
     class PeriodicWork : Worker() {
 
         override fun doWork(): WorkerResult {
-            Log.d("work", "check")
+            Timber.e("Tick work")
             return WorkerResult.SUCCESS
         }
     }
 
     private fun startWork() {
         val build = PeriodicWorkRequest.Builder(PeriodicWork::class.java, PeriodicWorkRequest.MIN_PERIODIC_INTERVAL_MILLIS, TimeUnit.MILLISECONDS)
-                .setConstraints(Constraints.Builder()
+               .addTag(PERIODIC_TASK_TAG)
+               .setConstraints(Constraints.Builder()
                         .setRequiredNetworkType(NetworkType.CONNECTED)
                         .build())
                 .build()
